63f74d3536350378a090ab09c7006efc23628c22
Jonas Bulik <jonas@bulik.dev>
2024-06-28 15:22:40 +0200

    type: Look up executables in PATH

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+fn find_command_in_path(cmd: &str) -> Option<String> {
+    let paths = std::env::var_os("PATH")?;
+    for path in std::env::split_paths(&paths) {
+        let full_path = format!("{}/{}", path.display(), cmd);
+        if std::fs::metadata(&full_path).is_ok() {
+            return Some(full_path);
+        }
+    }
+    None
+}
+
 fn main() {
     // Wait for user input
     let stdin = io::stdin();
@@ -19,7 +30,13 @@ fn main() {
                 for arg in args {
                     match *arg {
                         "exit" | "echo" | "type" => println!("{} is a shell builtin", arg),
-                        a => println!("{}: not found", a),
+                        a => {
+                            if let Some(path) = find_command_in_path(a) {
+                                println!("{}: {}", a, path);
+                            } else {
+                                println!("{}: not found", a);
+                            }
+                        }
                     }
                 }
             }
